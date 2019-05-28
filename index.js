d12e79b29d1eb3aa1ec5e46a57cef1796a32bedd
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-28 14:07:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+
+
+function justInvoke(functionName){
+return functionName();
+}
+function setThisWithCall(fn, thisValue, arg){
+return fn.call(thisValue, arg);
+}
+function setThisWithApply(fn, thisValue, args){
+return fn.apply(thisValue, args);
+}
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+	const copyFunction = functionToBeCopied.bind(thisValue);
+	return copyFunction;
+}
